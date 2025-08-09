7de47177527028841291283f4ebd01b46d6dec75
Joao Vitor <joaovitorlouzada@hotmail.com>
2025-08-09 18:33:35 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/joaovitorlouzada/course/entities/Order.java
+++ src/main/java/com/joaovitorlouzada/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.joaovitorlouzada.course.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
@@ -22,8 +23,13 @@ public class Order implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	
+	
 	private OrderStatus orderStatus;
+	
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
---
+++ src/main/java/com/joaovitorlouzada/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.joaovitorlouzada.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaovitorlouzada.course.entities.Order;
+import com.joaovitorlouzada.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
